2dd0d82d74649f827661aa0195d624d0a27bb6c8
Daniel Norambuena <norambuena921@gmail.com>
2023-04-03 19:32:23 -0400

    complementa

---
+++ src/Components/GaleriaComplementa.jsx
@@ -0,0 +1,78 @@
+import React from 'react'
+
+const GaleriaComplementa = () => {
+    return (
+        <section className="section-products">
+            <div className="container">
+
+                <div className="row">
+                    <div className="col-md-6 col-lg-4 col-xl-3">
+                        <div id="product-1" className="single-product">
+                            <div className="part-1">
+                                <ul>
+                                    <li><a href="#"><i className="fas fa-shopping-cart"></i></a></li>
+                                    <li><a href="#"><i className="fas fa-heart"></i></a></li>
+                                    <li><a href="#"><i className="fas fa-plus"></i></a></li>
+                                </ul>
+                                <img src='https://falabella.scene7.com/is/image/Falabella/50207835_3?wid=1500&hei=1500&qlt=70' style={{ width: '270px', height: '300px' }}></img>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="col-md-6 col-lg-4 col-xl-3">
+                        <div id="product-1" className="single-product">
+                            <div className="part-1">
+
+                                <ul>
+                                    <li><a href="#"><i className="fas fa-shopping-cart"></i></a></li>
+                                    <li><a href="#"><i className="fas fa-heart"></i></a></li>
+                                    <li><a href="#"><i className="fas fa-plus"></i></a></li>
+                                </ul>
+                                <img src='https://coliseumstorecl.vtexassets.com/arquivos/ids/677593-800-auto?v=637818206338030000&width=800&height=auto&aspect=true' style={{ width: '270px', height: '300px' }}></img>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="col-md-6 col-lg-4 col-xl-3">
+                        <div id="product-1" className="single-product">
+                            <div className="part-1">
+                                <ul>
+                                    <li><a href="#"><i className="fas fa-shopping-cart"></i></a></li>
+                                    <li><a href="#"><i className="fas fa-heart"></i></a></li>
+                                    <li><a href="#"><i className="fas fa-plus"></i></a></li>
+                                </ul>
+                                <img src='https://coliseumstorecl.vtexassets.com/arquivos/ids/677144/168816C_1.jpg?v=637811501661900000' id="imgrotate" style={{ width: '270px', height: '300px' }}></img>
+
+                            </div>
+
+                        </div>
+                    </div>
+
+                    <div className="col-md-6 col-lg-4 col-xl-3">
+                        <div id="product-1" className="single-product">
+                            <div className="part-1">
+
+                                <ul>
+                                    <li><a href="#"><i className="fas fa-shopping-cart"></i></a></li>
+                                    <li><a href="#"><i className="fas fa-heart"></i></a></li>
+                                    <li><a href="#"><i className="fas fa-plus"></i></a></li>
+                                </ul>
+                                <img src='https://falabella.scene7.com/is/image/Falabella/15774133_3?wid=1500&hei=1500&qlt=70' id="imgrotate" style={{ width: '300px' }}></img>
+
+                            </div>
+                        </div>
+                    </div>
+
+
+
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default GaleriaComplementa
--- src/Views/Descripcion.jsx
+++ src/Views/Descripcion.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Container, Row, Card, } from 'react-bootstrap';
 import Header from '../Components/Header';
+import GaleriaComplementa from '../Components/GaleriaComplementa';
 
 const Descripcion = () => {
   return (
@@ -34,67 +35,11 @@ const Descripcion = () => {
 
       <div className='contenedor-complementa'>
         <div className='crear-publi-complementa'>
-          <h4 style={{ marginBottom: '2em' }}>Complementa tu compra</h4> 
+          <h4 style={{ marginBottom: '10px' }}>Complementa tu compra</h4> 
         </div>
 
-        <Container className='d-flex' style={{ justifyContent: 'center' }}>
-          <Row md={3}>
+        <GaleriaComplementa></GaleriaComplementa>
 
-
-            <Card style={{ width: '18rem', marginRight: '2em', }}>
-              <Card.Img variant="top" className='publi-img' style={{ width: '90%', margin: '2em 1em 0em 1em ', borderRadius: '14px' }} src='https://ae01.alicdn.com/kf/S609b243f73564d4eb15377a58d1cff863/PMAWXS-Zapatillas-deportivas-transpirables-para-hombre-zapatos-blancos-ligeros-y-c-modos-para-caminar-zapatillas-de.jpg' />
-              <Card.Body>
-                <div style={{ textAlign: 'center' }}>
-                  <div >
-                  <div>
-                  <h4 style={{ marginBottom: '10px' }}>$0.000</h4>
-                </div>
-                    <button style={{ marginBottom: '0px', borderRadius: '16px', paddingRight: '2em', paddingLeft: '2em' }} type="button"
-                      class="btn btn-outline-dark">Detalle del producto</button>
-
-                  </div>
-                </div>
-              </Card.Body>
-            </Card>
-
-            <Card style={{ width: '18rem', marginRight: '2em', }}>
-              <Card.Img variant="top" className='publi-img' style={{ width: '90%', margin: '2em 1em 0em 1em ', borderRadius: '14px' }} src='https://ae01.alicdn.com/kf/S609b243f73564d4eb15377a58d1cff863/PMAWXS-Zapatillas-deportivas-transpirables-para-hombre-zapatos-blancos-ligeros-y-c-modos-para-caminar-zapatillas-de.jpg' />
-              <Card.Body>
-                <div style={{ textAlign: 'center' }}>
-                  <div >
-                  <div>
-                  <h4 style={{ marginBottom: '10px' }}>$0.000</h4>
-                </div>
-                    <button style={{ marginBottom: '0px', borderRadius: '16px', paddingRight: '2em', paddingLeft: '2em' }} type="button"
-                      class="btn btn-outline-dark">Detalle del producto</button>
-
-                  </div>
-                </div>
-              </Card.Body>
-            </Card>
-
-            <Card style={{ width: '18rem', marginRight: '2em', }}>
-              <Card.Img variant="top" className='publi-img' style={{ width: '90%', margin: '2em 1em 0em 1em ', borderRadius: '14px' }} src='https://ae01.alicdn.com/kf/S609b243f73564d4eb15377a58d1cff863/PMAWXS-Zapatillas-deportivas-transpirables-para-hombre-zapatos-blancos-ligeros-y-c-modos-para-caminar-zapatillas-de.jpg' />
-              <Card.Body>
-                <div style={{ textAlign: 'center' }}>
-                  <div >
-                  <div>
-                  <h4 style={{ marginBottom: '10px' }}>$0.000</h4>
-                </div>
-                    <button style={{ marginBottom: '0px', borderRadius: '16px', paddingRight: '2em', paddingLeft: '2em' }} type="button"
-                      class="btn btn-outline-dark">Detalle del producto</button>
-
-                  </div>
-                </div>
-              </Card.Body>
-            </Card>
-
-            
-
-
-            
-          </Row>
-        </Container>
       </div>
 
     </>

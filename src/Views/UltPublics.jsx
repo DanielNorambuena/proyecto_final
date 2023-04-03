f8c2aa4ac41e75cdd100c07f7a6171c773225448
dma27cl <dm_orrego@hotmail.com>
2023-04-03 18:51:50 -0400

    ultima actualizacion

--- src/Views/UltPublics.jsx
+++ src/Views/UltPublics.jsx
@@ -9,8 +9,28 @@ const UltPublics = () => {
       <Header></Header>
       <Container>
         <div>
-          <h1 style={{ fontSize: '20pt', marginTop: '2em', textAlign:'center' }}>Catálogo de productos</h1>
+          <h1 style={{ fontSize: '20pt', marginTop: '2em', textAlign: 'center' }}>Catálogo de productos</h1>
         </div>
+
+        <div className='sort'>
+          <div>
+          <p style={{marginRight:'1em', marginLeft:'1em'}}>Ordenar por:</p>
+          </div>
+          <div>
+          <select>
+            <option value="Menor a mayor">Menor a mayor</option>
+            <option value="Mayor a menor">Mayor a menor</option>
+            <option selected value="Mas recientes primero">Mas recientes primero</option>
+            <option value="Mas antiguos primero">Mas antiguos primero</option>
+          </select>
+          </div>
+          
+          {/* <div>
+            <input style={{ borderRadius: '14px', border: 'none', padding: '5px', marginTop: '1em' }} type="text" name="email" placeholder='Buscar en la tienda' />
+          </div> */}
+        </div>
+      
+
         <Galeria></Galeria>
       </Container >
 

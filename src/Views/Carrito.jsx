0c5a6c078cabfaf7931f30771d18d2e3248f2eef
Daniel Norambuena <norambuena921@gmail.com>
2023-04-04 18:31:21 -0400

    carrito

--- src/Views/Carrito.jsx
+++ src/Views/Carrito.jsx
@@ -1,8 +1,35 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Carrito = () => {
   return (
-    <div>Carrito</div>
+    <>
+      <div className='cart-container'>
+        <div className='cart-subcontainer'>
+          <h3>Detalles del pedido:</h3>
+          <div >
+            
+            <div className='cart'>
+              <img src='https://falabella.scene7.com/is/image/Falabella/15774133_3?wid=1500&hei=1500&qlt=70' alt="zapatillas"></img>
+              <NavLink style={{ textDecoration: 'none' }}>Zapatilla Urbana</NavLink>
+              <p>$9.990</p>
+
+              <p>$9.990</p>
+              <button > + </button>
+              <p>5</p>
+              <button > - </button>
+            </div>
+            <hr />
+          </div>
+
+
+          <h3> Total: $9.990</h3>
+
+        </div>
+      </div>
+    </>
+
+
   )
 }
 
